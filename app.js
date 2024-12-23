ad68dd40acfe5c1d592f7ecc07213f19023bc921
usashi16 <usashi003@gmail.com>
2024-12-23 21:52:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ let config = {
   let game = new Phaser.Game(config);
   
   function preload() {
-    this.load.image("background", "assets/background.png");
-    this.load.image("road", "assets/road.png");
-    this.load.image("column", "assets/column.png");
-    this.load.spritesheet("bird", "assets/bird.png", {
+    this.load.image("background", "background.png");
+    this.load.image("road", "road.png");
+    this.load.image("column", "column.png");
+    this.load.spritesheet("bird", "bird.png", {
       frameWidth: 64,
       frameHeight: 96,
     });
